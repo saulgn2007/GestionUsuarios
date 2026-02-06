2de81f45f79232dff37adfe1d059d9bf75b4517b
inesr <inesrz333@gmail.com>
2026-02-06 18:55:40 +0100

    Viajes usuario

---
+++ Git/src/git/Principal.java
@@ -0,0 +1,22 @@
+package git;
+import java.util.Scanner;
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Viajes datos = new Viajes();
+		double gastoTotal=0;
+		System.out.println("¿Cuántos viajes deseas guardar?");
+		int num= Integer.parseInt(scan.nextLine());
+		while(num!=0) {
+			datos.setDestino();
+			datos.setFechaIda();
+			datos.setFechaVuelta();
+			datos.setCoste();
+			System.out.println(datos.toString());
+			num--;
+			gastoTotal= gastoTotal+datos.getCoste();
+		}
+		scan.close();
+	}
+}
